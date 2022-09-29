f1503ec414cffadb73eaf31b22461a0efc728d1f
bweilers <ben.eilers@gmail.com>
2022-09-29 10:44:30 -0500

    modified pyramen.py

--- PyRamen/PyRamen.py
+++ PyRamen/PyRamen.py
@@ -6,15 +6,21 @@ import csv
 from pathlib import Path
 
 # @TODO: Set file paths for menu_data.csv and sales_data.csv
-menu_filepath = Path('')
-sales_filepath = Path('')
+menu_filepath = Path('Resources/menu_data.csv')
+sales_filepath = Path('Resources/sales_data.csv')
 
 # @TODO: Initialize list objects to hold our menu and sales data
 menu = []
 sales = []
 
 # @TODO: Read in the menu data into the menu list
+with open(menu_filepath, 'r') as file:
+    csvFile = csv.reader(file)
+    for line in csvFile:
+        # print(line)
+        menu.append(line)
 
+# print(menu)
 
 
 
@@ -25,7 +31,13 @@ sales = []
 
 
 # @TODO: Read in the sales data into the sales list
+with open(sales_filepath, 'r') as file:
+    csvFile = csv.reader(file)
+    for line in csvFile:
+        print(line)
+        sales.append(line)
 
+# print(sales)
 
 
 
@@ -42,17 +54,26 @@ report = {}
 row_count = 0
 
 # @TODO: Loop over every row in the sales list object
+for sale_row in sales:
 
 
 
 
     # Line_Item_ID,Date,Credit_Card_Number,Quantity,Menu_Item
     # @TODO: Initialize sales data variables
+    Line_Item_ID = sale_row[0]
+    Date = sale_row[1]
+    Credit_Card_Number = sale_row[2]
+    Quantity = sale_row[3]
+    Menu_Item = sale_row[4]
 
 
     # @TODO:
     # If the item value not in the report, add it as a new entry with initialized metrics
     # Naming convention allows the keys to be ordered in logical fashion, count, revenue, cost, profit
+    if Menu_Item not in report:
+        report[Menu_Item] = {'01-count': 0, '02-revenue': 0, '03-cogs': 0, '04-profit': 0}
+
 
 
 
@@ -62,21 +83,29 @@ row_count = 0
 
 
     # @TODO: For every row in our sales data, loop over the menu records to determine a match
+    for menu_row in menu:
 
 
         # Item,Category,Description,Price,Cost
         # @TODO: Initialize menu data variables
+        Item = menu_row[0]
+        Category = menu_row[1]
+        Description = menu_row[2]
+        Price = menu_row[3]
+        Cost = menu_row[4]
 
 
 
 
         # @TODO: Calculate profit of each item in the menu data
+        proft = Price - Cost
 
 
         # @TODO: If the item value in our sales data is equal to the any of the items in the menu, then begin tracking metrics for that item
-
+        if Menu_Item == Item:
 
             # @TODO: Print out matching menu data
+            print(Item)
 
 
 
@@ -93,11 +122,11 @@ row_count = 0
 
 
 
-    # @TODO: Increment the row counter by 1
-
+    # @TODO: Increment the sale_row counter by 1
+    row_count += 1
 
 # @TODO: Print total number of records in sales data
-
+print(row_count)
 
 
 
